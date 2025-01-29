49f7347cb3f3aa4f187d739f3e16cbb9d5fee658
wellinton de Souza <168542172+wellintondsouza@users.noreply.github.com>
2025-01-29 08:06:14 -0300

    Create Qestão B.cs

---
+++ Qestão B.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Projeto1
+{
+	class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Digite um numero:");
+			int num1 = int.Parse (Console.ReadLine());
+			
+				
+			int dobro = num1 * num1;
+			
+				
+				Console.WriteLine("A soma dos números é de:" + dobro);
+				Console.ReadLine();
+			
+				  Console.WriteLine("Aperte qualquer botão para fechar...");
+        Console.ReadKey(); // Aguarda que o usuário pressione uma tecla
+		}
+	}
